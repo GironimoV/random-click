f4694cd43e3744f982183065599030f5e3d1f68a
Vlad Gira <140818600+GironimoV@users.noreply.github.com>
2023-07-28 16:03:06 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function generateRandomColor() {
+    const randomColor = getRandomColor();
+    document.getElementById('colorBox').style.backgroundColor = randomColor;
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
